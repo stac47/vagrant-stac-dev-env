c9370b3a21f141d2885c2af54ee328209b243d0f
Laurent Stacul <laurent.stacul@gmail.com>
2020-12-03 21:52:27 +0100

    Upgrade to Ubuntu/Groovy

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
-
-set -o xtrace
-set -o nounset
-set -o errexit
+set -euo pipefail
+IFS=$'\n\t'
 
 STAC_DOT_FILES='.stac-dot-files'
-GO_VERSION='1.13.5'
-HOME_FOR_VAGRANT='/home/vagrant'
 
 apt-get update && apt-get dist-upgrade -y && apt-get install -y \
     apt-transport-https \
@@ -28,27 +24,10 @@ if [[ ! -d "${STAC_DOT_FILES}" ]]; then
 fi
 
 ln -sf ${STAC_DOT_FILES}/.zshrc .zshrc
-if [[ ! -e "${HOME_FOR_VAGRANT}/.zshenv" ]]; then
-    echo "source ${HOME_FOR_VAGRANT}/${STAC_DOT_FILES}/.zshenv" > "${HOME_FOR_VAGRANT}/.zshenv"
-fi
+ln -sf ${STAC_DOT_FILES}/.zshenv .zshenv
 ln -sf ${STAC_DOT_FILES}/.vimrc .vimrc
 ln -sf ${STAC_DOT_FILES}/.vim .vim
 ln -sf ${STAC_DOT_FILES}/.gitconfig .gitconfig
 ln -sf ${STAC_DOT_FILES}/.gitconfig_common .gitconfig_common
 ln -sf ${STAC_DOT_FILES}/.gitignore_global .gitignore_global
 ln -sf ${STAC_DOT_FILES}/.tmux.conf.linux .tmux.conf
-
-# Rust language
-if [[ ! -x $(command -v cargo) ]]; then
-    wget https://sh.rustup.rs -O rustup.sh && sh rustup.sh -y && rm rustup.sh
-fi
-
-# Go language
-if [[ ! -d '/usr/local/go' ]]; then
-    wget https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz
-    tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
-    rm go${GO_VERSION}.linux-amd64.tar.gz
-    echo "export PATH=/usr/local/go/bin:$PATH" >> ${HOME_FOR_VAGRANT}/.zshenv
-fi
-
-chown --recursive vagrant:vagrant ${HOME_FOR_VAGRANT}
